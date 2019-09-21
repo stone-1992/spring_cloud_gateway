372e060552e7051f69bb3fc874143d21f5b5bf0b
石宏俐 <hongli_shi@123cx.com>
2019-09-21 14:53:47 +0800

    提交

---
+++ api-gateway/src/main/java/com/stone/ApiCateWayApplication.java
@@ -0,0 +1,12 @@
+package com.stone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCateWayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCateWayApplication.class, args);
+	}
+}
---
+++ service-one/src/main/java/com/stone/controller/UserController.java
@@ -0,0 +1,18 @@
+package com.stone.controller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/user")
+@RestController
+public class UserController {
+	
+	@RequestMapping("who")
+	public String getUser() {
+		
+		return "this is user : service-one";
+	}
+	
+	
+}
